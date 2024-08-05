8f16a839364b5a88303508fb443347a281265f8c
James C <5689414+james-cnz@users.noreply.github.com>
2024-08-05 21:08:07 +1200

    MDL-82709 course: Prefer section IDs over nums

--- course/format/classes/output/local/content.php
+++ course/format/classes/output/local/content.php
@@ -155,7 +155,7 @@ class content implements named_templatable, renderable {
         // Generate section list.
         $sections = [];
         $stealthsections = [];
-        foreach ($this->get_sections_to_display($modinfo) as $sectionnum => $thissection) {
+        foreach ($this->get_sections_to_display($modinfo) as $thissection) {
             // The course/view.php check the section existence but the output can be called
             // from other parts so we need to check it.
             if (!$thissection) {
@@ -167,6 +167,7 @@ class content implements named_templatable, renderable {
                 continue;
             }
 
+            $sectionnum = $thissection->section;
             /** @var \core_courseformat\output\local\content\section $section */
             $section = new $this->sectionclass($format, $thissection);
 
