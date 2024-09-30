1b652c7bac617ba1c2516c76464c70356466c872
James C <5689414+james-cnz@users.noreply.github.com>
2024-09-30 14:15:19 +1300

    MDL-83307 course: Section return fixes

--- course/format/classes/output/local/content/cm/delegatedcontrolmenu.php
+++ course/format/classes/output/local/content/cm/delegatedcontrolmenu.php
@@ -329,7 +329,10 @@ class delegatedcontrolmenu extends basecontrolmenu {
         $usecomponents = $format->supports_components();
         $coursecontext = context_course::instance($course->id);
 
-        $baseurl = course_get_url($course, $sectionreturn);
+        $baseurl = course_get_url($course);
+        if (!is_null($sectionreturn)) {
+            $baseurl->param('sectionid', $format->get_sectionid());
+        }
         $baseurl->param('sesskey', sesskey());
 
         $cmbaseurl = new url('/course/mod.php');
@@ -376,9 +379,6 @@ class delegatedcontrolmenu extends basecontrolmenu {
         $availablevisibility = has_capability('moodle/course:sectionvisibility', $coursecontext, $user) && $parentsection->visible;
         if ($availablevisibility) {
             $url = clone($baseurl);
-            if (!is_null($sectionreturn)) {
-                $url->param('sr', $format->get_sectionid());
-            }
             $strhidefromothers = get_string('hidefromothers', 'format_' . $course->format);
             $strshowfromothers = get_string('showfromothers', 'format_' . $course->format);
             if ($section->visible) { // Show the hide/show eye.
