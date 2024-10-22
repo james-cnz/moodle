68fcba4ca5edba773f7022702d8179e9f72b2ed4
James C <5689414+james-cnz@users.noreply.github.com>
2024-10-22 19:29:45 +1300

    MDL-83511 course: Add activities by section ID

--- course/classes/output/activitychooserbutton.php
+++ course/classes/output/activitychooserbutton.php
@@ -73,6 +73,7 @@ class activitychooserbutton implements templatable, renderable {
 
         return (object)[
             'sectionnum' => $this->section->section,
+            'sectionid' => $this->section->id,
             'sectionreturn' => $this->sectionreturn ?? false,
             'modid' => $this->mod ? $this->mod->id : false,
             'activityname' => $this->mod ? $this->mod->get_formatted_name() : false,
